24c20cf616932a94dcfc413afbde28e789bc3c13
krishnakeshariya1 <keshariyakrishna8@gmail.com>
2025-11-06 14:51:11 +0530

    complete performance optimization section

--- Performance Optimization/script.js
+++ Performance Optimization/script.js
@@ -8,8 +8,23 @@ return function(...arg){
     },delay);
 };
 }
-input.addEventListener("input",debounce(function (det){
-    console.log(det)
-},1000));
+// input.addEventListener("input",debounce(function (det){
+//     console.log(det)
+// },1000));
 // example of debouncing 
 
+//example of throttle
+
+function thorttle(fnc,delay){
+    let timer = 0;
+    return function(...arg){
+        let now = Date.now();
+        if(now - timer >= delay){
+            timer = now;
+            fnc(...arg);
+        }
+    };
+}
+input.addEventListener("input", thorttle(function(details){
+    console.log("Hey");
+},2000))
