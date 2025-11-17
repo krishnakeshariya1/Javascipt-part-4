33e3081fc5ea1a97bed09235c5981ce1c0f9af70
krishnakeshariya1 <keshariyakrishna8@gmail.com>
2025-11-17 14:57:06 +0530

    completed

--- Live-Word-Counter/script.js
+++ Live-Word-Counter/script.js
@@ -2,23 +2,38 @@ const textArea = document.querySelector("#textarea");
 const wordCount = document.querySelector("#wordCount");
 const characterCount = document.querySelector("#characterCount");
 const wpm = document.querySelector("#wpm");
-const longestWord = document.querySelector("#longestWord");
-const shortestWord  = document.querySelector("#shortestWord");
+const longestWord = document.querySelector("#longestword");
+const shortestWord = document.querySelector("#shortestWord");
 
-textArea.addEventListener("input" ,()=>{
+textArea.addEventListener("input", () => {
     const text = textArea.value;
     //count character including spaces
     characterCount.textContent = text.length;
 
     // count words 
-    let word = 0
+    let word = [];
     const trimmedText = text.trim();
-    if(trimmedText !== ""){
-        word = trimmedText.split(/\s+/).length;
+    if (trimmedText !== "") {
+        word = trimmedText.split(/\s+/);
     }
-    wordCount.textContent = word;
+    wordCount.textContent = word.length;
 
     //calculate WPM
-    const seconds = (word /200)*60;
-    wpm.textContent = (seconds <60)? seconds.toFixed(1) +"sec " : (seconds/60).toFixed(2)+ "min";
+    const seconds = (word.length / 200) * 60;
+    wpm.textContent = (seconds < 60) ? seconds.toFixed(1) + "sec " : (seconds / 60).toFixed(2) + "min";
+
+    // ignore a an the
+    const ignoreword = ["a", "an", "the"];
+    const filteredWords = word.filter(w => !ignoreword.includes(w));
+
+    // longest word finding
+    let longest = "";
+    let shortest = null;
+    for (let w of filteredWords) {
+        if (longest.length < w.length) longest = w;
+        if (shortest === null || shortest.length > w.length) shortest = w
+    }
+    longestWord.textContent = longest || "";
+    shortestWord.textContent = shortest || "";
+
 })
