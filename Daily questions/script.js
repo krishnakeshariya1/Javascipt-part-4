983b5bee9b26492bd8aca359cc6256c58cf690fd
krishnakeshariya1 <keshariyakrishna8@gmail.com>
2025-11-21 13:12:44 +0530

    add questions

--- Daily questions/script.js
+++ Daily questions/script.js
@@ -15,46 +15,46 @@ const countDigit = (num, target) => {
 }
 console.log(countDigit(5544554545544, 5));
 
-const reverseNumber = (num)=>{
-    if(num === 0)return 0;
+const reverseNumber = (num) => {
+    if (num === 0) return 0;
 
     num = Math.trunc(Number(num));
     let rev = 0;
-    while(num > 0){
+    while (num > 0) {
         let digit = Math.floor(num % 10);
-        rev = rev *10 + digit;
-        num = Math.floor(num/ 10);
+        rev = rev * 10 + digit;
+        num = Math.floor(num / 10);
     }
     return rev
 }
 console.log(reverseNumber(8956))
 
-const anagramCheck = (str1 ,str2)=>{
-    if(str1.length !== str2.length) return false
+const anagramCheck = (str1, str2) => {
+    if (str1.length !== str2.length) return false
 
-    let freq ={}
-    for(let ch of str1){
-        freq[ch] = (freq[ch] || 0)+1
+    let freq = {}
+    for (let ch of str1) {
+        freq[ch] = (freq[ch] || 0) + 1
     }
-    for(let ch of str2){
-        if(!freq[ch]) return false
+    for (let ch of str2) {
+        if (!freq[ch]) return false
         freq[ch]--;
     }
 
     return true
 }
-console.log(anagramCheck("race","care"))
+console.log(anagramCheck("race", "care"))
 
-const secondLargest = (arr)=>{
+const secondLargest = (arr) => {
     let max = -Infinity;
     let secondLargest = -Infinity;
 
-    for(let char of arr){
-        if(max < char){
+    for (let char of arr) {
+        if (max < char) {
             secondLargest = max;
             max = char;
         }
-         else if(secondLargest < char && char < max){
+        else if (secondLargest < char && char < max) {
             secondLargest = char;
         }
     }
@@ -63,29 +63,29 @@ const secondLargest = (arr)=>{
         secondLargest
     }
 }
-console.log(secondLargest([10,5,20,8,20,7,16]));
+console.log(secondLargest([10, 5, 20, 8, 20, 7, 16]));
 console.log(secondLargest([-5, -10, -3, -3]));
 
 
-const removeDublicate=(arr)=>{
+const removeDublicate = (arr) => {
     const newArr = [];
-    
-    for(let char of arr){
+
+    for (let char of arr) {
         let exist = false;
 
-        for(let val of newArr){
-            if(val === char){
+        for (let val of newArr) {
+            if (val === char) {
                 exist = true;
                 break;
             }
         }
-        if(!exist){
+        if (!exist) {
             newArr.push(char)
         }
     }
     return newArr
 }
-console.log(removeDublicate([5,7,8,10,5,20,8,5]));
+console.log(removeDublicate([5, 7, 8, 10, 5, 20, 8, 5]));
 
 function smallestDigit(num) {
     let smallest = Infinity;
@@ -97,7 +97,7 @@ function smallestDigit(num) {
             smallest = digit;
         }
 
-        num = Math.floor(num / 10);   
+        num = Math.floor(num / 10);
     }
 
     return smallest;
@@ -117,7 +117,7 @@ function sumDigits(num) {
     return sum;
 }
 
-console.log(sumDigits(5432)); 
+console.log(sumDigits(5432));
 
 function isPalindrome(num) {
     let original = num;
@@ -126,7 +126,7 @@ function isPalindrome(num) {
     while (num > 0) {
         let digit = Math.floor(num % 10);
         rev = rev * 10 + digit;
-        num = Math.floor(num / 10);      
+        num = Math.floor(num / 10);
     }
 
     return original === rev
@@ -148,7 +148,7 @@ function isPalindrome(num) {
 }
 
 console.log(isPalindrome(90009)); // true
-function countEvenDigits(num){
+function countEvenDigits(num) {
     let count = 0;
 
     while (num > 0) {
@@ -159,7 +159,7 @@ function countEvenDigits(num){
     return count;
 }
 
-console.log(countEvenDigits(87654321)); 
+console.log(countEvenDigits(87654321));
 
 function countGreaterThan5(num) {
     let count = 0;
@@ -173,60 +173,60 @@ function countGreaterThan5(num) {
     return count;
 }
 
-function reverseString(str){
+function reverseString(str) {
     let rev = "";
 
-    for(let i = str.length-1; i >=0; i--){
-        rev+= str[i];
+    for (let i = str.length - 1; i >= 0; i--) {
+        rev += str[i];
     }
     return rev
 }
 console.log(reverseString("krishna"));
 
-function countOccurrences(arr , target){
+function countOccurrences(arr, target) {
     let count = 0;
 
-    for(let chr of arr){
-        if(chr === target) count++;
+    for (let chr of arr) {
+        if (chr === target) count++;
     }
     return count;
 }
-console.log(countOccurrences([2,4,5,2,3,5,6,2,9,0,2], 2)) // 4
+console.log(countOccurrences([2, 4, 5, 2, 3, 5, 6, 2, 9, 0, 2], 2)) // 4
 
-function findMax(arr){
-    if(arr.length === 0) return "Empty array"
+function findMax(arr) {
+    if (arr.length === 0) return "Empty array"
     let max = arr[0];
 
-    for(let char of arr){
-        if(max < char) max = char;
+    for (let char of arr) {
+        if (max < char) max = char;
     }
     return max
 }
-console.log(findMax([5,6,9,2,0,1]))
+console.log(findMax([5, 6, 9, 2, 0, 1]))
 
-function dublicateFind(arr){
+function dublicateFind(arr) {
     let result = [];
 
-    for(let char of arr){
+    for (let char of arr) {
         let exist = false;
-        for(let val of result){
-            if(val === char){
+        for (let val of result) {
+            if (val === char) {
                 exist = true;
                 break;
             }
         }
-        if(!exist){
+        if (!exist) {
             result.push(char);
         }
     }
     return result
 }
-console.log(dublicateFind([2,3,4,5,2,4,7,9,0]))
+console.log(dublicateFind([2, 3, 4, 5, 2, 4, 7, 9, 0]))
 
-function sumOfNumber(num){
+function sumOfNumber(num) {
     let sum = 0;
-    while(num >0){
-        let digit = num%10;
+    while (num > 0) {
+        let digit = num % 10;
         sum += digit;
         num = Math.floor(num / 10);
     }
@@ -234,10 +234,17 @@ function sumOfNumber(num){
 }
 console.log(sumOfNumber(54321))
 
-function isSorted(arr){
-    for(let i =0; i<arr.length-1; i++){
-        if(arr[i] > arr[i+1]) return false
+function isSorted(arr) {
+    for (let i = 0; i < arr.length - 1; i++) {
+        if (arr[i] > arr[i + 1]) return false
     }
     return true
 }
-console.log(isSorted([1,2,3,4,6,8,2]))
+console.log(isSorted([1, 2, 3, 4, 6, 8, 2]))
+
+let arr = [5, 7, 8, 9, 2, 1, 4];
+let sum = 0;
+for (let char of arr) {
+    sum += char;
+}
+console.log(`The sum of all elements are :- ${sum}`);
