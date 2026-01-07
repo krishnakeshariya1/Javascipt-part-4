9d02f95e03349683f2f0607e7dcd013b535fe68e
krishnakeshariya1 <keshariyakrishna8@gmail.com>
2026-01-07 18:42:58 +0530

    added addFirst function and Traverse function

--- Linked-List/script.js
+++ Linked-List/script.js
@@ -8,5 +8,20 @@ class LinkList {
     constructor(){
         this.head = null;
     }
-
+    addFirst(data){
+        const n1 = new Node(data);
+        n1.next = this.head;
+        this.head = n1
+    } traverse(){
+        let current = this.head;
+        while (current) {
+            console.log(current.data)
+            current = current.next;
+        }
+    }
 }
+const linkList = new LinkList()
+const n7 =  linkList.addFirst(5);
+const n6 = linkList.addFirst(10)
+const n5 = linkList.addFirst(15);
+const n4 = linkList.addFirst(20)
