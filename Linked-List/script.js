01125da9c520dac917fc4e3fee370dc879041a35
krishnakeshariya1 <keshariyakrishna8@gmail.com>
2026-01-07 18:49:31 +0530

    updatebyVal and searchbyVal function added

--- Linked-List/script.js
+++ Linked-List/script.js
@@ -5,23 +5,41 @@ class Node {
     }
 }
 class LinkList {
-    constructor(){
+    constructor() {
         this.head = null;
     }
-    addFirst(data){
+    addFirst(data) {
         const n1 = new Node(data);
         n1.next = this.head;
         this.head = n1
-    } traverse(){
+    } traverse() {
         let current = this.head;
         while (current) {
             console.log(current.data)
             current = current.next;
         }
     }
+    searchByVal(val) {
+        let current = this.head;
+        while (current.data !== val) {
+            current = current.next;
+        }
+        console.log(`We got the node ${current.data}`)
+    }
+    UpdateDataByVal(val, updateVal) {
+        let current = this.head;
+        while (current.data !== val) {
+            current = current.next;
+        }
+        current.data = updateVal;
+    }
 }
 const linkList = new LinkList()
-const n7 =  linkList.addFirst(5);
+const n7 = linkList.addFirst(5);
 const n6 = linkList.addFirst(10)
 const n5 = linkList.addFirst(15);
 const n4 = linkList.addFirst(20)
+linkList.traverse()
+linkList.searchByVal(10);
+linkList.UpdateDataByVal(15, 100);
+linkList.traverse()
